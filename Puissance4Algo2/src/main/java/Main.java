d813490a5c26de13f9b6f017351a7138c9b78803
Valentin-nasone <90779259+Valentin-nasone@users.noreply.github.com>
2023-04-07 19:03:47 +0200

    Ajout tmeps de réponse

--- Puissance4Algo2/src/main/java/Main.java
+++ Puissance4Algo2/src/main/java/Main.java
@@ -1,22 +1,31 @@
 public class Main {
 
     public static void main(String[] args) {
-        Puissance4 puissance4 = new Puissance4();
-        puissance4.init();
-        puissance4.set(3, 0, 2);
-        puissance4.set(3, 1, 2);
-        puissance4.set(3, 2, 1);
-        puissance4.set(2, 0, 1);
-        puissance4.set(2, 1, 1);
-        puissance4.set(1, 0, 1);
-        puissance4.set(4, 1, 2);
-        puissance4.set(3, 3, 1);
-        puissance4.set(4, 2, 2);
-        puissance4.set(4, 3, 1);
+        long startTime = System.nanoTime();
+
+            Puissance4 puissance4 = new Puissance4();
+            puissance4.init();
+            puissance4.set(3, 0, 2);
+            puissance4.set(3, 1, 2);
+            puissance4.set(3, 2, 1);
+            puissance4.set(2, 0, 1);
+            puissance4.set(2, 1, 1);
+            puissance4.set(1, 0, 1);
+            puissance4.set(4, 1, 2);
+            puissance4.set(3, 3, 1);
+            puissance4.set(4, 2, 2);
+            puissance4.set(4, 3, 1);
 
 
-        puissance4.showBoard();
-        System.out.println("Value board : "+puissance4.getValueBoard(2));
-        System.out.println("MOVE : " + puissance4.bestCoup(1));
+            puissance4.showBoard();
+            System.out.println("Value board : "+puissance4.getValueBoard(2));
+            System.out.println("MOVE : " + puissance4.bestCoup(1));
+
+
+        long endTime = System.nanoTime();
+
+        double executionTime = (endTime - startTime) / 1_000_000_000.0;
+        System.out.printf("Temps d'exécution : %.4f secondes%n", executionTime);
+
     }
 }
